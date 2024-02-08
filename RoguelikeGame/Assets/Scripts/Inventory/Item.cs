db14d38af0b132fc46f6987503c3215d6313d4fb
Sagonash <ryazanskiy.maxim@yandex.ru>
2024-02-08 14:49:31 +0300

    Added absctract class Item

---
+++ RoguelikeGame/Assets/Scripts/Inventory/Item.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public abstract class Item : MonoBehaviour
+    {
+        //protected int _Id;
+        protected string _Name;
+        protected uint _Price;
+        protected uint _Weight;
+        protected int _Durability;
+        struct Volume
+        {
+            public ushort _Length;
+            public ushort _Width;
+        };
+        Volume _Volume;
+    }
+}
