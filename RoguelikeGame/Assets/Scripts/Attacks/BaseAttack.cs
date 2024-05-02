442e1fd83371d9d756fee5c6998716e3c6c43668
xnuke2 <xmnikit@gmail.com>
2024-05-02 15:46:09 +0300

    Изменил класс BaseAttack

--- RoguelikeGame/Assets/Scripts/Attacks/BaseAttack.cs
+++ RoguelikeGame/Assets/Scripts/Attacks/BaseAttack.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class BaseAttack : MonoBehaviour
+public abstract class BaseAttack 
 {
-    [SerializeField] protected int _MinDamage;
-    [SerializeField] protected int _MaxDamage;
-    [SerializeField] protected int _Range;
-    [SerializeField] protected int _DelayTime;
-    [SerializeField] protected int _KnockBack;
-    abstract public void Attack();
+    protected Command PressHandler;
+    protected Command HoldHandler;
+    protected Command ReleaseHandler;
 }
+
+
+
+
+//[SerializeField] protected int _MinDamage;
+//[SerializeField] protected int _MaxDamage;
+//[SerializeField] protected int _Range;
+//[SerializeField] protected int _DelayTime;
+//[SerializeField] protected int _KnockBack;
