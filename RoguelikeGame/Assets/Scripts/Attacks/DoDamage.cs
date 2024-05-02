0a9874c88a144f0d44a2f295cc7498b7fc1daa8e
xnuke2 <xmnikit@gmail.com>
2024-05-02 15:48:26 +0300

    Добавлен класс DoDamage

---
+++ RoguelikeGame/Assets/Scripts/Attacks/DoDamage.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoDamage : Command
+{
+    float Coulddown;
+    float Knocknack;
+    int CurrentDamage;
+    int MinDamage;
+    int MaxDamage;
+}
